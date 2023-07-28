2770f9dc630af0a8490e92ac4397263276c563a4
Dat T Nguyen <n.dat@outlook.com>
2023-07-28 15:43:53 +0200

    run eur

--- run_EUR_cscqtl.sh
+++ run_EUR_cscqtl.sh
@@ -8,5 +8,18 @@
 #SBATCH --mail-type=ALL
 
 
+cd /mnt/project/Aqua-Faang/dat/cscqtl/cscQTL_dev
+
+
+reads="/mnt/ScratchProjects/Aqua-Faang/dat_projects/cscqtl_paper/data/filtered_rna_seq/*_{1,2}.fastq.gz"
+
+genotype='/mnt/ScratchProjects/Aqua-Faang/dat_projects/cscqtl_paper/data/all_eur_hg38.vcf.gz'
+
+meta='/mnt/ScratchProjects/Aqua-Faang/dat_projects/cscqtl_paper/data/meta_358_eur.csv'
+
+
+
+nextflow run main.nf -resume --outdir "EUR_358_samples" --reads $reads --genotype $genotype --meta $meta --consensus 3 --coloc true --circall true --ciri2 true --circexplorer2 true -with-report -profile cluster
+
 
 
